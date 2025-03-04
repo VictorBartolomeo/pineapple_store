00601c9bc24d7034e440cdb9c2e60da25373f95f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-04 13:40:56 +0100

    change method getProduct()

--- src/main/java/org/example/premier_projet_spring/controller/ProductController.java
+++ src/main/java/org/example/premier_projet_spring/controller/ProductController.java
@@ -3,28 +3,36 @@ package org.example.premier_projet_spring.controller;
 import org.example.premier_projet_spring.dao.ProductDao;
 import org.example.premier_projet_spring.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
 
-    @Autowired
     protected ProductDao productDao;
 
-    @GetMapping("/product")
-    public Product getProduct() {
+    @Autowired
+    public ProductController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 
-        Product coca = new Product();
+    @GetMapping("/product/{id}")
 
-        coca.setName("Pepsi");
-        coca.setId(1);
-        coca.setCode("COCA-PEPSI");
-        coca.setDescription("Pepsi, comme du Coca en meilleur");
-        coca.setPrice(2.15f);
-        return coca;
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+
+        Optional<Product> optionalProduct = productDao.findById(id);
+        if (optionalProduct.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalProduct.get(), HttpStatus.OK);
+
     }
 
 
