a35955500cd9d2fdff1141bfa1efc2ac32684ee4
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-04 10:50:45 +0100

    add database seeder

---
+++ src/main/resources/data-test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name, code, description, price)
+VALUES (1, 'Pineapple Watch', 'PINE1', 'Montre en ananas', 499.99),
+       (2, 'PinePhone', 'PINE2', 'A brand new generation Pineapple Phone', 999.99),
+       (3, 'PinePad Mini', 'PINE3', 'Next generation tablet Max performances, minimum size', 699.99),
+       (4, 'PinePad Pro', 'PINE4', 'Next generation tablet for designers and pros', 1299.99);
