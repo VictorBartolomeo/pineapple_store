93afd938d058c060293133f5b81cd3f80bb7a3fd
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-05 20:46:26 +0100

    seeding db with state values

--- src/main/resources/data-test-data.sql
+++ src/main/resources/data-test-data.sql
@@ -1,5 +1,13 @@
-INSERT INTO product (id, name, code, description, price)
-VALUES (1, 'Pineapple Watch', 'PINE1', 'Montre en ananas', 499.99),
-       (2, 'PinePhone', 'PINE2', 'A brand new generation Pineapple Phone', 999.99),
-       (3, 'PinePad Mini', 'PINE3', 'Next generation tablet Max performances, minimum size', 699.99),
-       (4, 'PinePad Pro', 'PINE4', 'Next generation tablet for designers and pros', 1299.99);
+INSERT INTO product (name, code, description, price)
+VALUES ('Pineapple Watch', 'PINE1', 'Montre en ananas', 499.99),
+       ('PinePhone', 'PINE2', 'A brand new generation Pineapple Phone', 999.99),
+       ('PinePad Mini', 'PINE3', 'Next generation tablet Max performances, minimum size', 699.99),
+       ('PinePad Pro', 'PINE4', 'Next generation tablet for designers and pros', 1299.99);
+
+INSERT INTO state (name)
+VALUES ('New'),
+       ('Like New'),
+       ('Used'),
+       ('Refurbished');
+
+
