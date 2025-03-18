940f56aae6a67732915b0f4fab6b1cbdea60e7d7
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-18 18:16:15 +0100

    add validations for product

--- src/main/java/org/example/premier_projet_spring/model/Product.java
+++ src/main/java/org/example/premier_projet_spring/model/Product.java
@@ -1,9 +1,17 @@
 package org.example.premier_projet_spring.model;
 
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.premier_projet_spring.view.PurchaseView;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,17 +23,34 @@ public class Product {
     protected Long id;
 
     @Column(nullable = false)
+    @JsonView(PurchaseView.class)
     protected String name;
 
     @Column(length = 10, nullable = false, unique = true)
+    @Length(min = 10, max = 10)
+    @NotBlank
     protected String code;
 
     @Column(columnDefinition = "TEXT")
+    @JsonView(PurchaseView.class)
     protected String description;
+
+    @DecimalMin(value = "0.1")
     protected float price;
-    
+
     @ManyToOne
+    @JoinColumn(nullable = false)
+    @JsonView(PurchaseView.class)
     protected State state;
+
+    @ManyToMany
+    @JoinTable(
+            name = "product_label",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "label_id")
+    )
+    @JsonView(PurchaseView.class)
+    protected List<Label> labels = new ArrayList<>();
 
 
 }
