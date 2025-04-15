87821442d882a2a86e96422e15d181deaf4b7795
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-15 13:38:57 +0200

    renaming jwtUtils to SecurityUtils and method getRole

--- src/main/java/org/example/premier_projet_spring/security/SecurityUtils.java
+++ src/main/java/org/example/premier_projet_spring/security/SecurityUtils.java
@@ -2,14 +2,23 @@ package org.example.premier_projet_spring.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
-public class JwtUtils {
+public class SecurityUtils {
+
+    public String getRole(AppUserDetails userDetails) {
+        return userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).findFirst().orElse(null);
+    }
 
     public String generateToken(AppUserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .addClaims(Map.of("role", getRole(userDetails)))
                 .signWith(SignatureAlgorithm.HS256, "secret")
                 .compact();
     }
