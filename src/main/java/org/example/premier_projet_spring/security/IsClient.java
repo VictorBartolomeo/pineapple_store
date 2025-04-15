ae75b9948e882914073a624dcdb0365e025bdc46
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-15 13:36:26 +0200

    adding role permissions

--- src/main/java/org/example/premier_projet_spring/security/IsClient.java
+++ src/main/java/org/example/premier_projet_spring/security/IsClient.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAnyRole('ROLE_CLIENT')")
+@PreAuthorize("hasAnyRole('ROLE_CLIENT', 'ROLE_SELLER', 'ROLE_CHIEF')")
 public @interface IsClient {
 
 }
