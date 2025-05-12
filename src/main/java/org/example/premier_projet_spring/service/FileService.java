3ee652173db12e9a986c47d17552a58a9ab689ef
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-12 16:58:57 +0200

    "Add support for separate public/private upload directories and implement image retrieval by name."

--- src/main/java/org/example/premier_projet_spring/service/FileService.java
+++ src/main/java/org/example/premier_projet_spring/service/FileService.java
@@ -1,9 +1,11 @@
 package org.example.premier_projet_spring.service;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -17,13 +19,16 @@ public class FileService {
     @Value("${public.upload.folder}")
     private String publicUploadFolder;
 
-    public void uploadToLocalFileSystem(MultipartFile file, String fileName) throws IOException {
-        uploadToLocalFileSystem(file.getInputStream(), fileName);
+    @Value("${private.upload.folder}")
+    private String privateUploadFolder;
+
+    public void uploadToLocalFileSystem(MultipartFile file, String fileName, boolean publicFile) throws IOException {
+        uploadToLocalFileSystem(file.getInputStream(), fileName, publicFile);
     }
 
-    public void uploadToLocalFileSystem(InputStream inputStream, String fileName) throws IOException {
+    public void uploadToLocalFileSystem(InputStream inputStream, String fileName, boolean publicFile) throws IOException {
 
-        Path storageDirectory = Paths.get(publicUploadFolder);
+        Path storageDirectory = Paths.get(publicFile ? publicUploadFolder : privateUploadFolder);
 
         if (!Files.exists(storageDirectory)) {
             try {
@@ -36,6 +41,18 @@ public class FileService {
         Path destination = Paths.get(storageDirectory.toString() + "/" + fileName);
 
         Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
+
+    }
+
+    public byte[] getImageByName(String nomImage) throws FileNotFoundException {
+
+        Path destination = Paths.get(privateUploadFolder + "/" + nomImage);// retrieve the image by its name
+
+        try {
+            return IOUtils.toByteArray(destination.toUri());
+        } catch (IOException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
 
     }
 }
