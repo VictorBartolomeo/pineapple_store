1a636c29a55f76e98b2f24d2da0e0d325cf3b83f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 09:44:14 +0200

    Add EmailValidationDto class with token and email fields

---
+++ src/main/java/org/example/premier_projet_spring/dto/EmailValidationDto.java
@@ -0,0 +1,13 @@
+package org.example.premier_projet_spring.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailValidationDto {
+
+    protected String token;
+    protected String email;
+//    protected String password;
+}
