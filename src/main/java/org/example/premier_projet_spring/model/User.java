224be15608111adc72181dad9e57969a8ad2feda
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 09:44:19 +0200

    "Add validation groups and token field to User model for enhanced input handling."

--- src/main/java/org/example/premier_projet_spring/model/User.java
+++ src/main/java/org/example/premier_projet_spring/model/User.java
@@ -9,22 +9,31 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.premier_projet_spring.view.ClientView;
 
+
 @Getter
 @Setter
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class User {
 
+    public interface ValidRegister {
+    }
+
+    public interface ValidUpdate {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonView(ClientView.class)
     protected Long id;
 
-    @NotBlank
+    @NotBlank(groups = {ValidRegister.class})
     @Email
     @Column(unique = true, nullable = false)
     @JsonView(ClientView.class)
     protected String email;
+
+    protected String validEmailToken;
 
     @NotBlank
     @Column(nullable = false)
